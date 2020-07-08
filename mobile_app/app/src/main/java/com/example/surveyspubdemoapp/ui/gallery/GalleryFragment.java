a5aa79f7d79a9b4ac204c3e6d9df767aa44e219e
Martin Contreras <mcontre@google.com>
2020-07-07 22:03:55 -0500

    Added content to the third page of website and started adding content to third page of mobile app

---
+++ mobile_app/app/src/main/java/com/example/surveyspubdemoapp/ui/gallery/GalleryFragment.java
@@ -0,0 +1,26 @@
+package com.example.surveyspubdemoapp.ui.gallery;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.surveyspubdemoapp.R;
+
+public class GalleryFragment extends Fragment {
+
+    private GalleryViewModel galleryViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_gallery, container, false);
+        return root;
+    }
+}
